91e35c61c31ff02589b7e5874f34f71608b8b2fb
kei-0021 <quita02@gmail.com>
2025-09-26 05:28:29 +0000

    issue #1 初期データをJSONファイルから読み込めるようにしました

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,7 @@
 // src/main.tsx
 import { createRoot } from "react-dom/client";
 import Scoreboard from "./ScoreBoard.js";
-
-console.log("main.tsx loaded");
-
-const playersData = [
-  { 
-    id: "p1",
-    name: "Alice", 
-    score: 12, 
-    cards: [
-      { id: "1", name: "1", description: "このカードを使うと1マス進める" },
-      { id: "2", name: "2", description: "このカードを使うと2マス進める" }] 
-  },
-  { id: "p2", name: "Bob", score: 20, cards: [] },
-  { id: "p3", name: "Charlie", score: 8, cards: [] },
-];
+import playersData from "./data/players.json";
 
 const rootEl = document.getElementById("root");
 
@@ -23,9 +9,9 @@ if (rootEl) {
   const root = createRoot(rootEl);
   root.render(
     <Scoreboard 
-        players={playersData}
-        sortByScore={true}
-        currentPlayerId="p2" // 今のターンのプレイヤーID
-    />);
-  console.log("Scoreboard rendered");
+      players={playersData}
+      sortByScore={true}
+      currentPlayerId="p2"
+    />
+  );
 }
