5da8d53247d4aa5dfed0d287e7ca52d0c9108b53
kei-0021 <quita02@gmail.com>
2025-09-28 02:31:15 +0000

    issue #2 index.tsをsrc下に持ってきました

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// src/index.ts
+
+// コンポーネント
+export { default as Deck } from './components/Deck.js';
+export { default as Dice } from './components/Dice.js';
+export { default as ScoreBoard } from './components/ScoreBoard.js';
+export { default as Timer } from './components/Timer.js';
+
+// 型
+export * from './types/card.js';
+export * from './types/player.js';
+
--- src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./card.js";
-export * from "./player.js";
-
